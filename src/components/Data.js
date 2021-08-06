cd8cf39bc2910d34a3efec6e7e29ef8a529746dd
Sourabh Bucha <sourabhking14@gmail.com>
2021-08-06 22:54:46 +0530

    data

---
+++ src/components/Data.js
@@ -0,0 +1,168 @@
+export const Data = [{   
+      id: "1",  
+      title: "Milne Hai Mujhse Aayi",
+      artist: "Arjit Singh",
+      img_source: "./images/Milne-Hai-Mujhse-Aayi.jpg",
+      src: "./music/Milne-Hai-Mujhse-Aayi.m4a",
+      playlist: true 
+    },
+    {   
+      id : "2",
+      title : "Bulleya",
+      artist : "Pritam",
+      img_source : "./images/Bulleya.jpg",
+      src : "./music/bulleya.mp3",
+      playlist: false 
+    },
+    {
+      id : "3",
+      title : "Roop Tera Mastana",
+      artist : "Sanam(Band)",
+      img_source : "./images/Roop-Tera-Mastana-Sanam.jpg",
+      src : "./music/Roop-Tera-Mastana-Sanam.m4a",
+      playlist: false 
+    },
+    {
+      id : "4",
+      title : "Tum",
+      artist : "Atif Aslam",
+      img_source : "./images/Tum.jpg",
+      src : "./music/Tum.mp3",
+      playlist: false 
+    },
+    {
+      id : "5",
+      title : "Bhula Dena",
+      artist : "Mustafa Zahid",
+      img_source : "./images/Bhula-Dena.jpg",
+      src : "./music/Bhula-Dena.m4a",
+      playlist: false 
+    },
+    {
+      id: "6",
+      title: "Past life",
+      artist: "Trevor Daniel & Selena Gomez",
+      img_source: "./images/Past-life.jpg",
+      src: "./music/Past-life.mp3",
+      playlist: false
+    },
+    {
+      id: "7",
+      title: "Dance to this",
+      artist: "Troye Sivan feat. Ariana Grande",
+      img_source: "./images/Dance-to-this.jpg",
+      src: "./music/Dance-to-this.mp3",
+      playlist: false
+    },
+    {
+      id: "8",
+      title: "Maniac",
+      artist: "Conan Gray",
+      img_source: "./images/Maniac.jpg",
+      src: "./music/Maniac.mp3",
+      playlist: false
+    },
+    {
+      id: "9",
+      title: "Emily",
+      artist: "Jeremy Zucker & Chelsea Cutler",
+      img_source: "./images/Emily.jpg",
+      src: "./music/Emily.mp3",
+      playlist: false 
+    },
+    {
+      id: "10",
+      title: "You’re welcome",
+      artist: "Jordan Fisher feat. Lin-Manuel Miranda",
+      img_source: "./images/You_re-welcome.jpg",
+      src: "./music/You_re-welcome.mp3",
+      playlist: false
+    },
+    {
+      id: "11",
+      title: "Used to be",
+      artist: "Arrows to Athens",
+      img_source: "./images/Used-to-be.jpg",
+      src: "./music/Used-to-be.mp3",
+      playlist: false
+    },
+    {
+      id: "12",
+      title: "Graveyard",
+      artist: "Halsey",
+      img_source: "./images/Graveyard.jpg",
+      src: "./music/Graveyard.mp3",
+      playlist: false 
+    },
+    {
+      id: "13",
+      title: "One call away",
+      artist: "Charlie Puth",
+      img_source: "./images/One-call-away.jpg",
+      src: "./music/One-call-away.mp3",
+      playlist: false 
+    },
+    {
+      id: "14",
+      title: "Take your time",
+      artist: "Sam Hunt",
+      img_source: "./images/Take-your-time.jpg",
+      src: "./music/Take-your-time.mp3",
+      playlist: false 
+    },
+    {
+      id: "15",
+      title: "Slow Dance In A Parking Lot",
+      artist: "Jordan Davis",
+      img_source: "./images/Slow-dance-in-a-parking-lot.jpg",
+      src: "./music/Slow-dance-in-a-parking-lot.mp3",
+      playlist: false 
+    },
+    {
+      id: "16",
+      title: "There’s no way",
+      artist: "Lauv & Julia Michaels",
+      img_source: "./images/There_s-no-way.jpg",
+      src: "./music/There_s-no-way.mp3",
+      playlist: false 
+    },
+    {
+      id: "17",
+      title: "Our Song",
+      artist: "Anne-Marie & Niall Horan",
+      img_source: "./images/Our-song.jpg",
+      src: "./music/Our-song.mp3",
+      playlist: false 
+    },
+    {
+      id: "18",
+      title: "Eastside",
+      artist: "Benny Blanco & Halsey & Khalid",
+      img_source: "./images/Eastside.jpg",
+      src: "./music/Eastside.mp3",
+      playlist: false 
+    },
+    {
+      id: "19",
+      title: "Sweet escape",
+      artist: "Alesso & Sirena",
+      img_source: "./images/Sweet-escape.jpg",
+      src: "./music/Sweet-escape.mp3",
+      playlist: false 
+    },
+    {
+      id: "20",
+      title: "Dandelions",
+      artist: "Ruth B.",
+      img_source: "./images/Dandelions.jpg",
+      src: "./music/Dandelions.mp3",
+      playlist: false 
+    },
+    {
+      id: "21",
+      title: "End game",
+      artist: "Taylor Swift (Feat. Ed Sheeran & Future)",
+      img_source: "./images/End-game.jpg",
+      src: "./music/End-game.mp3",
+      playlist: false 
+    }]
